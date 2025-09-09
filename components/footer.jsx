2eaeae04f47588029eab9d315610f14803b49c3c
prannnjal_ <pranjalkumar445@gmail.com>
2025-09-10 00:30:49 +0530

    Remove Jarvis integration and update copyright to 2025

--- components/footer.jsx
+++ components/footer.jsx
@@ -102,7 +102,7 @@ export default function Footer() {
         <div className="border-t border-white/10 mt-12 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-white/60 text-sm font-generalsans">
-              © 2024 Xylotek. All rights reserved.
+              © 2025 Xylotek. All rights reserved.
             </p>
             <div className="flex space-x-6 mt-4 md:mt-0">
               <a href="#" className="text-white/60 hover:text-white text-sm transition-colors font-generalsans">
