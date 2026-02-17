90f987a1ea80a1817b622353ec8cfb54e6890cac
prannnjal_ <pranjalkumar445@gmail.com>
2026-02-18 00:12:08 +0530

    final2

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,7 +3,7 @@ export const dynamic = 'force-static'
 export default function sitemap() {
     return [
         {
-            url: 'https://xylotek.com',
+            url: 'https://xylotek.com/',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
