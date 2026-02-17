1c930396b562a4e9dfcda5bb221f02795f95eb96
prannnjal_ <pranjalkumar445@gmail.com>
2026-02-18 00:05:13 +0530

    final2

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+        },
+        sitemap: 'https://xylotek.com/sitemap.xml',
+    }
+}
