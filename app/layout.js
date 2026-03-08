317a7791c914e92b9c722da766228655f30948bc
prannnjal_ <pranjalkumar445@gmail.com>
2026-03-08 20:30:13 +0530

    feat

--- app/layout.js
+++ app/layout.js
@@ -59,9 +59,44 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Xylotek",
+              "url": "https://xylotek.in",
+              "logo": "https://xylotek.in/logo.png",
+              "sameAs": [
+                "https://www.linkedin.com/company/xylotek",
+                "https://twitter.com/xylotek",
+                "https://www.facebook.com/xylotek"
+              ]
+            })
+          }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Xylotek",
+              "url": "https://xylotek.in",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://xylotek.in/search?q={search_term_string}",
+                "query-input": "required name=search_term_string"
+              }
+            })
+          }}
+        />
         <meta name="theme-color" content="#000000" />
       </head>
       <body className={`${inter.className} bg-black text-white antialiased`}>
