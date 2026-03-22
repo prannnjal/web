e7f7c6b8a703e18407a6822b6bb9fdb648e6e85b
prannnjal_ <pranjalkumar445@gmail.com>
2026-03-23 00:49:09 +0530

    fix:ad

--- app/robots.js
+++ app/robots.js
@@ -2,10 +2,16 @@ export const dynamic = 'force-static'
 
 export default function robots() {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'Googlebot',
+                allow: '/ads.txt',
+            }
+        ],
         sitemap: 'https://xylotek.in/sitemap.xml',
     }
 }
